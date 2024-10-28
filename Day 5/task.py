1ed347db1fe7c28ff9f52892b5744014557e75e9
danny8823 <dannyyoo714@gmail.com>
2024-10-27 22:08:43 -0700

    Fnished Day 5 project, did not finish hard level

---
+++ Day 5/task.py
@@ -0,0 +1,30 @@
+# fruits = ['Apple', 'Peach', 'Pear']
+
+# for fruit in fruits:
+#     print("THIS FRUIT IS CALLED: " + fruit)
+
+student_scores = [150, 142, 185, 120, 161,
+                  171, 170, 24, 59, 68, 199, 78, 65, 89, 220]
+
+# total_exam_score = sum(student_scores)
+
+# print(total_exam_score)
+
+# total_students = 0
+# sum = 0
+# for score in student_scores:
+#     total_students += 1
+#     sum += score
+
+# current_score = 0
+
+# for score in student_scores:
+#     if score > current_score:
+#         current_score = score
+#         print(current_score)
+
+total = 0
+for number in range(1, 101):
+    total += number
+
+print(total)
