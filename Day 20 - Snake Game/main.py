874c9ceb8eff70a8976736abe79690a3d4733088
danny8823 <dannyyoo714@gmail.com>
2024-11-14 12:37:00 -0800

    Created food for snake game

--- Day 20 - Snake Game/main.py
+++ Day 20 - Snake Game/main.py
@@ -1,5 +1,6 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title('My snake game')
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 
@@ -24,4 +26,7 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # DETECT collison
+    if snake.head.distance(food) < 15:
+        food.refresh()
 screen.exitonclick()
