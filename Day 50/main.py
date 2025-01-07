d9d17cf84c409a86b5c3cdcff74cb01d92652a85
danny8823 <dannyyoo714@gmail.com>
2025-01-07 14:27:07 -0800

    Finished Day 50

--- Day 50/main.py
+++ Day 50/main.py
@@ -25,17 +25,3 @@ print('sleep over')
 get_speed_int = driver.find_element(
     by=By.CLASS_NAME, value='download-speed')
 print(get_speed_int.text)
-# def get_speed_int():
-#     print('start')
-#     get_speed_button = driver.find_element(
-#         by=By.CLASS_NAME, value='js-start-test')
-#     time.sleep(120)
-#     print('sleep over')
-#     get_speed_button.click()
-#     time.sleep(240)
-#     get_speed_int = driver.find_element(
-#         by=By.CLASS_NAME, value='result-data-large number result-data-value download-speed')
-#     print(get_speed_int)
-
-
-# get_speed_int()
