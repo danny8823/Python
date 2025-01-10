ef17b7d4f9370d6cd775e023bd3f385a15e7b202
danny8823 <dannyyoo714@gmail.com>
2025-01-10 15:32:34 -0800

    Finished

--- Day 53/main.py
+++ Day 53/main.py
@@ -31,11 +31,6 @@ with open('/Users/danny714/Desktop/Python/Day 53/zillow_html.txt', 'r') as file:
         property_address_price.append(price.text.strip())
 
 
-print(len(property_address_link))
-print(len(property_address_string))
-print(len(property_address_price))
-
-
 class ZillowForm:
     def __init__(self):
         self.driver = webdriver.Chrome(options=chrome_options)
@@ -44,12 +39,35 @@ class ZillowForm:
         self.driver.get(
             "https://forms.gle/JaxL3e17sUoP4qWZ7")
         time.sleep(3)
-        inputs = self.driver.find_elements(
-            By.CLASS_NAME, value='whsOnd')
 
-        inputs[0].send_keys(property_address_string[0])
-        inputs[1].send_keys(property_address_price[0])
-        inputs[2].send_keys(property_address_link[0])
+        for num in range(len(property_address_link)):
+            inputs = self.driver.find_elements(
+                By.CLASS_NAME, value='whsOnd')
+            address_input = inputs[0]
+            price_input = inputs[1]
+            link_input = inputs[2]
+
+            if num == range(len(property_address_link)):
+                print('done')
+
+            time.sleep(2)
+            address_input.send_keys(property_address_string[num])
+            price_input.send_keys(property_address_price[num])
+            link_input.send_keys(property_address_link[num])
+
+            time.sleep(2)
+
+            submit = self.driver.find_element(
+                By.XPATH, value='/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div[1]/div')
+
+            submit.click()
+
+            time.sleep(2)
+
+            submit_another_response = self.driver.find_element(
+                By.XPATH, value='/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+
+            submit_another_response.click()
 
 
 bot = ZillowForm()
